5ca5bb4067eafbb20c6117752fdab6d573705f93
Venkatesh Bharadwaj Srinivasan <hcsclass10@gmail.com>
2025-12-14 08:15:10 +0000

    Syntactical edits

--- cosine_similarity.swift
+++ cosine_similarity.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-func cosine(_ v1: [Double], _ v2: [Double]) -> Double{
+func cosine(_ v1: [Float], _ v2: [Float]) -> Float{
   guard v1.count == v2.count, !v1.isEmpty else {
     return 0.0
   }
 
-  var dotpdt: Double = 0.0
-  var mag_a: Double = 0.0
-  var mag_b: Double = 0.0
+  var dotpdt: Float = 0.0
+  var mag_a: Float = 0.0
+  var mag_b: Float = 0.0
 
   for i in 0..<v1.count{
     dotpdt += v1[i] * v2[i]
@@ -23,8 +23,8 @@ func cosine(_ v1: [Double], _ v2: [Double]) -> Double{
 }
 
 
-var v1: Array = [1.0,2.0,3.0,4.0,5.0]
-var v2: Array = [5.0,6.0,7.0,8.0,9.0]
+var v1: [Float] = [1.0,2.0,3.0,4.0,5.0]
+var v2: [Float] = [5.0,6.0,7.0,8.0,9.0]
 
 var val = cosine(v1, v2)
-print(val)
+print("The cosine similarity is: \(val)")
