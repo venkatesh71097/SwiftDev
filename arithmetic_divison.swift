f7b12f6fb1593da122d6da667d9c5aab24cecbc5
Venkatesh Bharadwaj Srinivasan <hcsclass10@gmail.com>
2025-12-16 09:25:32 -0800

    Make division function return optional Float
    Updated division function to handle zero division by returning nil instead of zero.

--- arithmetic_divison.swift
+++ arithmetic_divison.swift
@@ -1,9 +1,21 @@
-func division(n: Float, factor: Float) -> Float {
+// Making return type of Float optional - that way, we can return Float as well as Nil (Python's None)
+func division(n: Float, factor: Float) -> Float? {
   guard factor != 0 else {
-    return 0 // making it zero for ZeroDivisionError
+    print("Zero division error")
+    return nil
   }
   return n / factor
 }
 
-var x = division(n: 10, factor: 2) 
+var x = division(n: 10, factor: 2)
 print("Output is \(x)")
+
+// func division(n: Float, factor: Float) -> Float {
+//   guard factor != 0 else {
+//     return 0 // making it zero for ZeroDivisionError
+//   }
+//   return n / factor
+// }
+
+// var x = division(n: 10, factor: 2) 
+// print("Output is \(x)")
